c95f39efa5d5e0399370e480e5915c5c8164f3cc
Gramatskiy Nikita <nik98.champion@gmail.com>
2019-01-07 21:37:10 +0200

    Try front

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
+from django.views.generic import TemplateView
 
 from accounts import views
 from rest_framework import routers
@@ -11,6 +12,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('myapp/', include('myapp.urls')),
     path('account/', include('accounts.urls')),
-    path('myapp/', include('myapp.urls')),
+    #path('account/', TemplateView.as_view(template_name='index.html')),
     path('docs/', include_docs_urls(title='Couse API', permission_classes=[])),
 ]
