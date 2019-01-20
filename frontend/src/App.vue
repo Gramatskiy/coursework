beee3830b01ec4440ab370ddcbc8f80fb073cae8
marakaci <mykyta.chernenko@nure.ua>
2019-01-20 11:19:43 +0200

    vue corrected

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@ import json
 json.loads(request.body.decode('utf-8'))
 <template>
   <div class="container">
-      <form>
+    <form @submit.prevent="postProducts">
 
       <div class="well">
         <h4> Add product</h4>
@@ -16,70 +16,75 @@ json.loads(request.body.decode('utf-8'))
         </div>
       </div>
 
-      <button @click="postProducts">Post</button>
+      <button>Post</button>
     </form>
     <div>
-    <ul>
-    <li v-for="product in products">
-    {{product.id + product.name}}
-    </li>
-    </ul>
-    <button @click="getProducts">Get products</button>
+      <ul>
+        <li v-for="product in products">
+          {{product.id + product.name}}
+        </li>
+      </ul>
+      <button @click="getProducts">Get products</button>
     </div>
   </div>
 </template>
 
 
 <script>
-import axios from 'axios';
-export default {
-  name: 'hello',
-  data() {
-    return {
-      products: [],
-      msg: 'Welcome to Your Vue.js App',
-      product: { name: '', price: ''},
-    }
-  },
-  xsrfHeaderName: "X-CSRFToken",
-  methods: {
-    postProducts() {
-      let newProduct = {
-      name: this.product.name,
-      price: this.product.price
-      }
+  import axios from 'axios';
 
-      console.log(newProduct);
-      axios.post("http://localhost:8000/myapp/product", newProduct)
-      .then((response)=>{
-        console.log(response);
-        })
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        products: [],
+        msg: 'Welcome to Your Vue.js App',
+        product: {name: '', price: ''},
+      }
     },
-    getProducts() {
-      axios.get("http://localhost:8000/myapp/product").then(res => this.products = res.data);
+    created() {
+      this.getProducts();
     },
-    getOrganizations() {
-      axios.get("http://localhost:8000/myapp/organization").then(res => this.products = res.data);
+    methods: {
+      postProducts() {
+        const newProduct = {
+          name: this.product.name,
+          price: this.product.price
+        };
+
+        console.log(newProduct);
+        axios.post("http://localhost:8000/myapp/product/", newProduct)
+          .then(() => this.getProducts())
+          .catch(reason => console.error(reason.response.data))
+      },
+      getProducts() {
+        axios.get("http://localhost:8000/myapp/product/").then(res => this.products = res.data)
+      },
+      getOrganizations() {
+        axios.get("http://localhost:8000/myapp/organization/").then(res => this.products = res.data)
+      }
     }
   }
-}
 
 </script>
 
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
